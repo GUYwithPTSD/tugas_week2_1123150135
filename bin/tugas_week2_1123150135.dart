8d579448f2957b8303e2918e16628ddb1fb7c6c4
GUYwithPTSD <ivandarma3000@gmail.com>
2025-09-27 10:11:28 +0700

    first commit kalkulator

---
+++ bin/tugas_week2_1123150135.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_week2_1123150135/tugas_week2_1123150135.dart' as tugas_week2_1123150135;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_week2_1123150135.calculate()}!');
+}
---
+++ test/tugas_week2_1123150135_test.dart
@@ -0,0 +1,8 @@
+import 'package:tugas_week2_1123150135/tugas_week2_1123150135.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
